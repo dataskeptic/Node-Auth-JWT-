c74c975aaeffabbd7e58d269011695e06d557015
mathenrifs <mateus_henrique2011@outlook.com>
2021-03-19 19:43:12 -0300

    mongoose validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,26 @@
+const User = require('../models/User');
+
+// handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password:''};
+    
+    // Duplicade error code
+    if(err.code === 11000) {
+        errors.email = 'that email is already registered';
+        return errors;
+    }
+
+    
+    // Validation errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message
+        })
+    }
+    return errors;
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -6,17 +29,25 @@ module.exports.login_get = (req, res) => {
     res.render('login');
 }
 
-module.exports.signup_post = (req, res) => {
+// Create new User
+module.exports.signup_post = async (req, res) => {
+
     const { email, password } = req.body;
 
-    console.log(email, password);
-    res.send('new signup');
+    try {
+        const user = await User.create({ email, password });
+        res.status(201).json(user);
+    } 
+    catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
 }
 
 module.exports.login_post = (req, res) => {
+    
     const { email, password } = req.body;
 
     console.log(email, password);
     res.send('User login');
 }
-
